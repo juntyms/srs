4fa993ea3ed2ddbab3fb32ce2ee571af8475d278
zainab-dev1 <zainab.alawaid1@gmail.com>
2023-01-08 10:14:19 +0400

    Migrations & Requests

--- app/Http/Requests/softwareRequest.php
+++ app/Http/Requests/softwareRequest.php
@@ -24,9 +24,7 @@ class softwareRequest extends FormRequest
     public function rules()
     {
         return [    'ay_id'=>'required','department_id'=>'required','name'=>['required'],'software_vendor_id'=>'required',
-                    'software_type_id'=>'required','company_id'=>'required','purchase_date'=>'required','expiry_date'=>'required',
-                    'warranty_end_date'=>'required','license_id'=>'required','installer_is_available'=>'required',
-                    'custodian_name'=>['required']
+                    'software_type_id'=>'required','company_id'=>'required','license_id'=>'required'
         ];
     }
 
@@ -38,12 +36,7 @@ class softwareRequest extends FormRequest
                 'software_vendor_id.required'=>'Software Vendor can not be blank',
                 'software_type_id.required'=>'Software Type can not be blank',
                 'company_id.required'=>'Company name can not be blank',
-                'purchase_date.required'=>'Purchase date can not be blank',
-                'expiry_date.required'=>'Expiry Date can not be blank',
-                'warranty_end_date.required'=>'Warranty End Date can not be blank',
                 'license_id.required'=>'License name can not be blank',
-                'installer_is_available.required'=>'Please choose if installer available or not',
-                'custodian_name.required'=>'Custodian name can not be blank',
                 ];
     }
 }
